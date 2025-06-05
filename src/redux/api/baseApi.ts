1318b5656d51da58fb0de7227ebfa5663af97da7
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-05 15:33:31 +0600

    api   🐳

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
-  // baseUrl: "http://localhost:5000/api",
-  baseUrl: "https://nh-social-server-site.vercel.app/api",
+  // Base Api
+   baseUrl: "http://localhost:5000/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
